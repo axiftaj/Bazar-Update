6dcb542991eef427766fac1ebb347a4402ed1a8d
Asif Taj <47206155+axiftaj@users.noreply.github.com>
2022-03-28 21:39:19 +0500

    Initial
    Initial

---
+++ lib/view/utilz/route_name.dart
@@ -0,0 +1,12 @@
+
+class RouteName {
+
+  static const String splashScreen = 'splash';
+  static const String option = 'option';
+  static const String login = 'login';
+
+  //buyer dashboard route name
+  static const String buyerDashboard = 'buyer_dashboard';
+
+
+}
